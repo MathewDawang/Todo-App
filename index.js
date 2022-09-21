54c415d1e81763e5d0370a86c6ce7166d7d41e1c
MathewDawang <dawangmannaw@gmail.com>
2022-09-21 04:40:20 +0000

    added input form to Todo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let express = require("express"),
 TodoRouter =require("./route/todo")
+TodoStyles = require("./public/style")
 app = express();
 app.use("/", TodoRouter)
 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/views"))
+app.use(express.static(__dirname + "/public"))
 
 app.listen(5000, ()=>{
    console.log("app is listening") 
